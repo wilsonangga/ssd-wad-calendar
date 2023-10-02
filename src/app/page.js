326cd86fbdc530fd0981904cb6fc377157333df8
wilsonangga <angcungsen@gmail.com>
2023-10-02 13:38:33 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,181 @@
-import Image from 'next/image'
+"use client"
+import React, { useState, useEffect, useRef } from 'react';
 
-export default function Home() {
+function App() {
+  const [clicked, setClicked] = useState(null);
+  const [events, setEvents] = useState(
+    localStorage.getItem('events')
+      ? JSON.parse(localStorage.getItem('events'))
+      : []
+  );
+  const [eventText, setEventText] = useState('');
+  const [eventTitleInput, setEventTitleInput] = useState('');
+  const [eventInviteesInput, setEventInviteesInput] = useState('');
+  const [eventTimeInput, setEventTimeInput] = useState('');
+  const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  const calendarRef = useRef(null);
+  const newEventModalRef = useRef(null);
+  const modalBackDropRef = useRef(null);
+  const deleteEventModalRef = useRef(null)
+
+  const openModal = (date) => {
+    setClicked(date);
+    const eventForDay = events.find((e) => e.date === date);
+    if (eventForDay) {
+      setEventText(eventForDay.title);
+      deleteEventModalRef.current.style.display = 'block'
+    } else {
+      newEventModalRef.current.style.display = 'block';
+    }
+    modalBackDropRef.current.style.display = 'block'
+  };
+
+  const closeModal = () => {
+    newEventModalRef.current.style.display = 'none';
+    deleteEventModalRef.current.style.display = 'none'
+    modalBackDropRef.current.style.display = 'none'
+    setEventTitleInput('');
+    setClicked(null);
+  };
+
+  const saveEvent = () => {
+    if (eventTitleInput) {
+      const updatedEvents = [...events];
+      updatedEvents.push({
+        date: clicked,
+        title: eventTitleInput,
+        invitees: eventInviteesInput,
+        time: eventTimeInput
+      });
+      localStorage.setItem('events', JSON.stringify(updatedEvents));
+      setEvents(updatedEvents);
+      newEventModalRef.current.style.display = 'none';
+      setEventTitleInput('');
+      modalBackDropRef.current.style.display = 'none'
+      window.location.reload();
+    }
+  };
+
+  const deleteEvent = () => {
+    const updatedEvents = events.filter((e) => e.date !== clicked);
+    localStorage.setItem('events', JSON.stringify(updatedEvents));
+    setEvents(updatedEvents);
+    deleteEventModalRef.current.style.display = 'none'
+    modalBackDropRef.current.style.display = 'none'
+    setClicked(null);
+    window.location.reload();
+  };
+
+  const loadCalendar = () => {
+    const dt = new Date();
+
+    const day = dt.getDate();
+    const month = dt.getMonth();
+    const year = dt.getFullYear();
+
+    const firstDayOfMonth = new Date(year, month, 1);
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+    const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+    });
+    const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+
+    const calendar = calendarRef.current;
+
+    for (let i = 1; i <= paddingDays + daysInMonth; i++) {
+      const daySquare = document.createElement('div');
+      daySquare.classList.add('day');
+
+      const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+
+      if (i > paddingDays) {
+        daySquare.innerText = i - paddingDays;
+        const eventForDay = events.find((e) => e.date === dayString);
+
+        if (i - paddingDays === day) {
+          daySquare.id = 'currentDay';
+        }
+
+        if (eventForDay) {
+          const eventDiv = document.createElement('div');
+          eventDiv.classList.add('event');
+          eventDiv.innerHTML = eventForDay.title + "<br />" + eventForDay.invitees + "<br />" + eventForDay.time;
+          daySquare.appendChild(eventDiv);
+        }
+
+        daySquare.addEventListener('click', () => openModal(dayString));
+        // daySquare.addEventListener('click', () => console.log("click"));
+      } else {
+        daySquare.classList.add('padding');
+      }
+
+      calendar.appendChild(daySquare);
+    }
+  }
+
+  useEffect(() => {
+    if (calendarRef.current.children.length === 0) {
+      loadCalendar()
+    }
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="App">
+      <div id="container">
+
+        <div id="weekdays">
+          <div>Sunday</div>
+          <div>Monday</div>
+          <div>Tuesday</div>
+          <div>Wednesday</div>
+          <div>Thursday</div>
+          <div>Friday</div>
+          <div>Saturday</div>
         </div>
+
+        <div id="calendar" ref={calendarRef}></div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div id="newEventModal" ref={newEventModalRef}>
+        <h2>New Event</h2>
+        <input
+          id="eventTitleInput"
+          placeholder="Event Title"
+          value={eventTitleInput}
+          onChange={(e) => setEventTitleInput(e.target.value)}
         />
+        <input
+          id="eventInviteesInput"
+          placeholder="Event Invitess"
+          value={eventInviteesInput}
+          onChange={(e) => setEventInviteesInput(e.target.value)}
+        />
+        <input
+          type="time"
+          id="eventTimeInput"
+          placeholder="Event Time"
+          value={eventTimeInput}
+          onChange={(e) => setEventTimeInput(e.target.value)}
+        />
+        <button id="saveButton" onClick={saveEvent}>Save</button>
+        <button id="cancelButton" onClick={closeModal}>Cancel</button>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div id="deleteEventModal" ref={deleteEventModalRef}>
+        <h2>Event</h2>
+        <p id="eventText">{eventText}</p>
+        <button id="deleteButton" onClick={deleteEvent}>Delete</button>
+        <button id="closeButton" onClick={closeModal}>Close</button>
       </div>
-    </main>
-  )
+
+      <div id="modalBackDrop" ref={modalBackDropRef}></div>
+    </div>
+  );
 }
+
+export default App;
